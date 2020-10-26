eeae782d8b70f334b219a9033b8d4f8b215750ce
Craaftx <guilmain.dorian@gmail.com>
2020-10-26 07:01:36 +0100

    :tada: Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  siteMetadata: {
+    title: `Guilmain Dorian`,
+    description: `Learn more about me by exploring my personnal website created with Gatsby.js and a lot of CSS.`,
+    author: `@craaftx`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Inter`,
+            variants: [`400`, `500`, `800`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        name: `Guilmain Dorian - Porfolio`,
+        short_name: `Dorian G`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        background_color: `#1d1d2d`,
+        theme_color: `#1d1d2d`,
+        display: `minimal-ui`,
+      },
+    },
+  ],
+}
